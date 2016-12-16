47e511dae69b32917f56233b181b7b863a0b1c5c
Pete Schwamb <pete@schwamb.net>
2016-12-16 13:07:49 -0600

    Add switch for debug mode

--- run.py
+++ run.py
@@ -4,5 +4,6 @@ import os
 
 port = int(os.environ.get("PORT", 5000))
 app = main.create_app('app.config.Base')
-app.config['DEBUG'] = True
+port = os.environ.get("PORT")
+app.config['DEBUG'] = os.environ.get("PRODUCTION_MODE", "False") != "True"
 app.run(host='0.0.0.0', port=port)
