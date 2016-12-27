206b50116b2cd625fd15b3de492d50dfdd247a04
Pete Schwamb <pete@schwamb.net>
2016-12-27 14:40:31 -0600

    starting mapping api

--- app/api.py
+++ app/api.py
@@ -12,8 +12,9 @@ api_auth = APIAuthWrapper()
 @api.route('/api/v1/sensor_proximity_events', methods=['POST'])
 @api_auth.requires_auth
 def post_sensor_proximity_events():
-    # TODO: auth
     event_data = request.get_json()
+    if not event_data:
+        abort(400)
     if not isinstance(event_data, list):
         event_data = [event_data]
     for event in event_data:
@@ -27,4 +28,13 @@ def post_sensor_proximity_events():
     return "%d" % len(event_data)
 
 # Sensor Mapping API #
-#@app.route('/api/v1/sensor_mappings', methods=['POST'])
+@api.route('/api/v1/sensor_mappings', methods=['POST'])
+@api_auth.requires_auth
+def create_sensor_mapping():
+    map_data = request.get_json()
+    if not map_data:
+        abort(400)
+    dev = SensorMapping(map_data.classroom_id, )
+    db.session.add(dev)
+    db.session.commit()
+    return jsonify( { 'developer': dev } ), 201
--- app/api_test.py
+++ app/api_test.py
@@ -1,12 +1,10 @@
-import os
-import api
 import unittest
-import tempfile
 import datetime
 import json
 from main import create_app
 from base64 import b64encode
 from auth_service import AuthCheckResult
+from models import *
 
 class MockAuthService():
     def check_auth(self, username, password):
@@ -15,31 +13,46 @@ class MockAuthService():
         else:
             return AuthCheckResult(False)
 
-proximity_event = data=dict(
-    school_id=1,
-    local_id=1,
-    remote_id=2,
-    observed_at=datetime.datetime.now().isoformat(),
-)
-
 class ApiTestCase(unittest.TestCase):
     def setUp(self):
-        # creates a test client
+        # Configure app and create a test_client
         app = create_app('app.config.TestConfig')
+        app.app_context().push()
         self.app = app.test_client()
+
         # propagate the exceptions to the test client
         self.app.testing = True
         app.config['DEBUG'] = True
+
         # Use mock auth service
         app.config["API_AUTH_SERVICE"] = MockAuthService()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
     def test_upload_without_auth(self):
+
+        proximity_event = data=dict(
+            school_id=1,
+            local_id=1,
+            remote_id=2,
+            observed_at=datetime.datetime.now().isoformat(),
+        )
+
         event_data = json.dumps([proximity_event])
         result = self.app.post('/api/v1/sensor_proximity_events',
             data=event_data, follow_redirects=True, content_type='application/json')
         self.assertEqual(result.status_code, 401)
 
     def test_upload_with_auth(self):
+        proximity_event = data=dict(
+            school_id=1,
+            local_id=1,
+            remote_id=2,
+            observed_at=datetime.datetime.now().isoformat(),
+        )
         headers = {
             'Authorization': 'Basic ' + b64encode("testuser:testpass")
         }
@@ -48,6 +61,11 @@ class ApiTestCase(unittest.TestCase):
             data=event_data, follow_redirects=True,
             content_type='application/json', headers=headers)
         self.assertEqual(result.status_code, 200)
+        events = SensorProximityEvent.query.all()
+        self.assertEqual(len(events), 1)
+
+    def test_mapping_create(self):
+        
 
 if __name__ == '__main__':
     unittest.main()
