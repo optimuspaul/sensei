3f3d76bd3285799d4f8fc77d8e3748960c312be4
Pete Schwamb <pete@schwamb.net>
2017-01-20 15:22:48 -0600

    add cors support

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from tc_auth_service import TCAuthService
 from models import db
 from api import api
 from api.api_json import APIJSONEncoder
+from flask_cors import CORS
 
 def create_app(config_obj):
     app = Flask(__name__)
+    CORS(app, resources=r'/api/*')
     app.config.from_object(config_obj)
     app.json_encoder = APIJSONEncoder
     db.init_app(app)
