13d15a677c1c7cfb2d3b6d1526d0c1aeb02d208a
Pete Schwamb <pete@schwamb.net>
2017-01-28 20:35:39 -0600

    fix db name

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ from tc_service import TCService
 
 class Base(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', "postgresql://localhost/sensei_new")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', "postgresql://localhost/sensei")
     TC_URL = os.getenv('TC_URL', 'http://localhost:3000')
     API_AUTH_SERVICE = TCAuthService(TC_URL)
     TC_SERVICE = TCService(TC_URL)
