15f40e0b0a87aca5b2655bcfc9e3e490fa619f55
Pete Schwamb <pete@schwamb.net>
2017-01-28 19:09:50 -0600

    remove unneeded columns from radio obs, and avoid posting duplicate obs

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from tc_auth_service import TCAuthService
-from models import db
+from models import db,migrate
 from api import api
 from api.api_json import APIJSONEncoder
 from flask_cors import CORS
 
 def create_app(config_obj):
-    app = Flask(__name__)
+    app = Flask('sensei')
     CORS(app, resources=r'/api/*')
     app.config.from_object(config_obj)
     app.json_encoder = APIJSONEncoder
     db.init_app(app)
+    migrate.init_app(app,db)
     app.register_blueprint(api)
     with app.app_context():
         db.create_all()
