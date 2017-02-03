7bfc95880a7199b69cd8a10e74c776927e572ddf
Mark Nutter <marknutter@gmail.com>
2017-02-03 14:29:41 -0600

    potential fix for sensor nav not showing up for classroom 725 (#32)
    * potential fix for sensor nav not showing up for classroom 725
    
    * actually fixes the problem
    
    * Revert "actually fixes the problem"
    
    This reverts commit c340dece4099d4a9860b0f121242b3984b6bae7e.
    
    * Revert "potential fix for sensor nav not showing up for classroom 725"
    
    This reverts commit f6f8140e7f1b7206cb8fe28438e7b5aebe48c7ad.
    
    * applied only the changes needed
    
    * sublime mucked up index.js, fixed it

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,123 +9,122 @@ import store from './store/configureStore';
 import { Provider } from 'react-redux';
 import {fetchMappings} from './actions/sensorMappingActions';
 import {fetchChildren, fetchTeachers, fetchEntities} from './actions/entityActions';
-import {getClassroomId} from './constants';
+import {getClassroomId, env} from './constants';
 import {fetchObservations} from './actions/insightsActions';
 import _ from 'lodash';
-
 import './index.css';
-import activityTimeline from './visualizations/activityTimeline'
+import activityTimeline from './visualizations/activityTimeline';
 
-// TODO: real routing.
+
 setTimeout(function(){
 
-  if (location.pathname.indexOf('wf/events') !== -1) {
-    let dynamicSecondaryNav = document.createElement("div");
-    dynamicSecondaryNav.className = "secondary-nav-link";
-    let secondaryNav = document.querySelector('.secondary-nav');
-    secondaryNav && secondaryNav.insertBefore(dynamicSecondaryNav, document.querySelector('.secondary-nav .clear'))
-    ReactDOM.render(
-      <SubNav/>,
-      dynamicSecondaryNav
-    )
+  // check to see if school is using sensors. hard coded to classroom ID 725 for now
+  let classroomId = getClassroomId();
+  if (classroomId === "725"  || env === 'development') {
 
-    let dynamicPrimaryNav = document.createElement("a");
-    dynamicPrimaryNav.className = "primary-nav-link";
-    let classroomId = getClassroomId();
-    dynamicPrimaryNav.href = `/networks/wf/events/sensors${classroomId ? '?classroom_id=' + classroomId : '' }`;
-    dynamicPrimaryNav.innerHTML = `<i class="fa fa-cubes"></i><span>Sensors</span>`
-    document.querySelector('.primary-nav').appendChild(dynamicPrimaryNav);
-  }
+      let dynamicPrimaryNav = document.createElement("a");
+      dynamicPrimaryNav.className = "primary-nav-link";
+      dynamicPrimaryNav.href = `/networks/wf/events/sensors${classroomId ? '?classroom_id=' + classroomId : '' }`;
+      dynamicPrimaryNav.innerHTML = `<i class="fa fa-cubes"></i><span>Sensors</span>`
+      document.querySelector('.primary-nav').appendChild(dynamicPrimaryNav);
 
-
-  if (location.pathname.indexOf('wf/events/sensors') !== -1) {
-    document.title = 'Sensors';
-
-    ReactDOM.render(
-      <Provider store={store}>
-        <SensorMappingInterfaceContainer/>
-      </Provider>,
-      document.getElementById('foundation')
-    );
-
-    store.dispatch(fetchChildren());
-    store.dispatch(fetchTeachers());
-    store.dispatch(fetchEntities('areas'));
-    store.dispatch(fetchEntities('materials'));
-    store.dispatch(fetchMappings());
-
-  }
-
-  if (location.pathname.indexOf('wf/events/entities') !== -1) {
-    document.title = 'Manage Materials & Areas';
-
-    ReactDOM.render(
-      <Provider store={store}>
-        <ManageEntitiesInterfaceContainer/>
-      </Provider>,
-      document.getElementById('foundation')
-    );
-
-    store.dispatch(fetchEntities('areas'));
-    store.dispatch(fetchEntities('materials'));
-
-  }
-
-  if (location.pathname.indexOf('wf/events/insights') !== -1) {
+    if (location.pathname.indexOf('wf/events') !== -1) {
+      let dynamicSecondaryNav = document.createElement("div");
+      dynamicSecondaryNav.className = "secondary-nav-link";
+      let secondaryNav = document.querySelector('.secondary-nav');
+      secondaryNav && secondaryNav.insertBefore(dynamicSecondaryNav, document.querySelector('.secondary-nav .clear'))
+      ReactDOM.render(
+        <SubNav/>,
+        dynamicSecondaryNav
+      )
 
 
+    }
 
-    let foundationEl = document.querySelector("#foundation");
-    foundationEl.innerHTML = `
-      <div class='row'>
-        <div class='col-md-2' id='insights-nav-container'></div>
-        <div class='col-md-10'>
-          <h2 id='visualization-title'></h2>
-          <hr />
-          <div id='visualization'><svg></svg></div>
+
+    if (location.pathname.indexOf('wf/events/sensors') !== -1) {
+      document.title = 'Sensors';
+
+      ReactDOM.render(
+        <Provider store={store}>
+          <SensorMappingInterfaceContainer/>
+        </Provider>,
+        document.getElementById('foundation')
+      );
+
+      store.dispatch(fetchChildren());
+      store.dispatch(fetchTeachers());
+      store.dispatch(fetchEntities('areas'));
+      store.dispatch(fetchEntities('materials'));
+      store.dispatch(fetchMappings());
+
+    }
+
+    if (location.pathname.indexOf('wf/events/entities') !== -1) {
+      document.title = 'Manage Materials & Areas';
+
+      ReactDOM.render(
+        <Provider store={store}>
+          <ManageEntitiesInterfaceContainer/>
+        </Provider>,
+        document.getElementById('foundation')
+      );
+
+      store.dispatch(fetchEntities('areas'));
+      store.dispatch(fetchEntities('materials'));
+
+    }
+
+    if (location.pathname.indexOf('wf/events/insights') !== -1) {
+
+
+
+      let foundationEl = document.querySelector("#foundation");
+      foundationEl.innerHTML = `
+        <div class='row'>
+          <div class='col-md-2' id='insights-nav-container'></div>
+          <div class='col-md-10'>
+            <h2 id='visualization-title'></h2>
+            <hr />
+            <div id='visualization'><svg></svg></div>
+          </div>
         </div>
-      </div>
-    `;
+      `;
 
-    ReactDOM.render(
-      <Provider store={store}>
-        <ActivityTimelineControlsContainer/>
-      </Provider>,
-      document.getElementById('insights-nav-container')
-    );
+      ReactDOM.render(
+        <Provider store={store}>
+          <ActivityTimelineControlsContainer/>
+        </Provider>,
+        document.getElementById('insights-nav-container')
+      );
 
 
-    store.dispatch(fetchChildren());
-    store.dispatch(fetchTeachers());
-    store.dispatch(fetchEntities('areas'));
-    store.dispatch(fetchEntities('materials'));
+      store.dispatch(fetchChildren());
+      store.dispatch(fetchTeachers());
+      store.dispatch(fetchEntities('areas'));
+      store.dispatch(fetchEntities('materials'));
 
-    let prevChildId, prevDate;
+      let prevChildId, prevDate;
 
-    store.subscribe(() => {
-      let state = store.getState();
-      let childId = _.get(state, 'insights.ui.currentChildId');
-      let date = _.get(state, 'insights.ui.currentDate');
+      store.subscribe(() => {
+        let state = store.getState();
+        let childId = _.get(state, 'insights.ui.currentChildId');
+        let date = _.get(state, 'insights.ui.currentDate');
 
-      if (childId && date) {
-        if (childId === prevChildId && date === prevDate) {
-          let child = _.get(state, `entities.children[${childId}]`);
-          let dateString = (new Date(date)).toDateString();
-          document.querySelector("#visualization-title").innerHTML = `${child.displayName} <small>${dateString}</small>`
-          activityTimeline(state.insights.observations[childId]);
-        } else {
-          document.querySelector("#visualization").innerHTML = '<h3>loading...</h3>';
-          store.dispatch(fetchObservations(childId, date));
-          prevDate = date;
-          prevChildId = childId;
+        if (childId && date) {
+          if (childId === prevChildId && date === prevDate) {
+            let child = _.get(state, `entities.children[${childId}]`);
+            let dateString = (new Date(date)).toDateString();
+            document.querySelector("#visualization-title").innerHTML = `${child.displayName} <small>${dateString}</small>`
+            activityTimeline(state.insights.observations[childId]);
+          } else {
+            document.querySelector("#visualization").innerHTML = '<h3>loading...</h3>';
+            store.dispatch(fetchObservations(childId, date));
+            prevDate = date;
+            prevChildId = childId;
+          }
         }
-      }
-
-    })
-
-
-
+      })
+    }
   }
-
-
 }, 200);
