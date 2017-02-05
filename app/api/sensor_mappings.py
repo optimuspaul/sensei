204f1277a86cd15135dc1eb911100c80fb1f0796
Pete Schwamb <pete@schwamb.net>
2017-02-05 13:38:30 -0500

    major bug in updating sensor mappings... other classrooms' mappings were getting deleted.

--- app/api/sensor_mappings.py
+++ app/api/sensor_mappings.py
@@ -25,9 +25,10 @@ def create_sensor_mapping():
         sensor_id = mapping.get('sensor_id')
         entity_type = mapping.get('entity_type')
         entity_id = mapping.get('entity_id')
+        classroom_id = mapping.get('classroom_id')
         now = datetime.datetime.now()
-        mappings_to_end = SensorMapping.query.filter_by(sensor_id=sensor_id,end_time=None).all()
-        mappings_to_end.extend(SensorMapping.query.filter_by(entity_id=entity_id,entity_type=entity_type,end_time=None))
+        mappings_to_end = SensorMapping.query.filter_by(classroom_id=classroom_id,sensor_id=sensor_id,end_time=None).all()
+        mappings_to_end.extend(SensorMapping.query.filter_by(classroom_id=classroom_id,entity_id=entity_id,entity_type=entity_type,end_time=None))
         for m in mappings_to_end:
             m.end_time = now
 
