a237f267126a743fc19d7ed7134c21b90965c518
Pete Schwamb <pete@schwamb.net>
2017-02-06 16:02:20 -0600

    increase width of graphs

--- frontend/src/visualizations/activityTimeline.js
+++ frontend/src/visualizations/activityTimeline.js
@@ -4,7 +4,7 @@ import store from './../store/configureStore';
 import _ from 'lodash';
 
 const ROW_HEIGHT = 30; // how tall each row of data in timeline is
-const STATIC_WIDTH = 800; // how wide the width of the visualization is
+const STATIC_WIDTH = 1260; // how wide the width of the visualization is
 const OFFSET = 205; // how far to the right the observation points start being drawn
 const VISUALIZATION_TEMPLATE = `
   <svg>
@@ -28,8 +28,8 @@ export default function activityTimeline(data) {
   document.querySelector("#visualization").innerHTML = VISUALIZATION_TEMPLATE;
 
   /*
-    Creates array of pairs that determine where the vertical timeline ticks 
-    are drawn and which hour label should be placed at the bottom of them to 
+    Creates array of pairs that determine where the vertical timeline ticks
+    are drawn and which hour label should be placed at the bottom of them to
     be fed into d3
    */
   let startTime = new Date(d3.min(data.timestamps));
@@ -57,7 +57,7 @@ export default function activityTimeline(data) {
 
 
   /*
-    Creates an object containing the data necessary for drawing the observation 
+    Creates an object containing the data necessary for drawing the observation
     points provided for each entity
    */
   let segmentedData = _.reduce(data.entities, (current, entity_data, index) => {
@@ -72,11 +72,11 @@ export default function activityTimeline(data) {
     return current;
   }, {});
 
-  
+
 
   /*
-    Creates a scalar on the x axis that constrains all values to the 
-    fixed width of the visualization, specified by the STATIC_WIDTH 
+    Creates a scalar on the x axis that constrains all values to the
+    fixed width of the visualization, specified by the STATIC_WIDTH
     constant defined above
    */
   var xScalar = d3.scaleLinear()
@@ -84,32 +84,32 @@ export default function activityTimeline(data) {
     .range([0, STATIC_WIDTH-OFFSET]);
 
   /*
-    Determines the height of the chart based on the number of entities included 
-    with the observational data and the predetermined ROW_HEIGHT constant defined 
-    above, with an extra ROW_HEIGHT's worth of space added to the bottom to make 
+    Determines the height of the chart based on the number of entities included
+    with the observational data and the predetermined ROW_HEIGHT constant defined
+    above, with an extra ROW_HEIGHT's worth of space added to the bottom to make
     space for the time tick labels
    */
   let chartHeight = (ROW_HEIGHT * (data.entities.length + _.size(segmentedData))) + ROW_HEIGHT;
 
 
   /*
-    Initializes the chart with d3 using the STATIC_WIDTH constant defined above 
+    Initializes the chart with d3 using the STATIC_WIDTH constant defined above
     and the calculated chartHeight from above
    */
   let chart = d3.select("#visualization svg")
                   .attr("width", STATIC_WIDTH)
                   .attr("height", chartHeight);
 
-  
+
 
   /*
-    Adds the dashed time tick lines and their appropriate hour labels 
-    using the ticks data array created above and scaled using the linear 
+    Adds the dashed time tick lines and their appropriate hour labels
+    using the ticks data array created above and scaled using the linear
     scalar xScalar created above to ensure they stay within the SVG's width
    */
   let ticksContainer = chart.select('#ticks');
 
-  ticksContainer.selectAll("line") 
+  ticksContainer.selectAll("line")
        .data(ticks)
        .enter().append("line")
        .attr("x1", (tick, index) => { return xScalar(tick[1]) + OFFSET + 15 })
@@ -117,20 +117,20 @@ export default function activityTimeline(data) {
        .attr("y1", 20)
        .attr("y2", chartHeight - 30);
 
-  ticksContainer.selectAll("text") 
+  ticksContainer.selectAll("text")
        .data(ticks)
        .enter().append("text")
        .attr("x", (tick, index) => { return xScalar(tick[1]) + OFFSET })
        .attr("y", chartHeight - 10)
        .text((tick, index) => { return parseInt(tick[0], 10) > 12 ? `${parseInt(tick[0], 10) - 12}:00pm` : `${tick[0]}:00${tick[0] === '12' ? 'pm' : 'am'}` })
-       
+
 
   // builds each entity type section using the segmentedData generated above
   _.each(segmentedData, buildSection);
 
   /*
-    Builds each entity row in the timeline visualization by taking the entity data 
-    and entity type provided and creating a label and plotting the observations using 
+    Builds each entity row in the timeline visualization by taking the entity data
+    and entity type provided and creating a label and plotting the observations using
     d3 and scaled linearly to fit the SVG's fixed width
    */
   function buildSection(entityData, entityType) {
@@ -142,14 +142,14 @@ export default function activityTimeline(data) {
     section.attr("transform", "translate(0," + ((entityData.y * ROW_HEIGHT)) + ")");
 
     // adds the entity type label for the current entity group
-    section.append("text") 
+    section.append("text")
            .attr("x", 0)
            .attr("y", 0)
            .attr("style", "font-weight: bold")
            .text(entityType);
 
     /*
-      adds a row for each entity included in the current entity type group and sets 
+      adds a row for each entity included in the current entity type group and sets
       its correct y displacement
      */
     let row = section.selectAll("g")
@@ -166,15 +166,15 @@ export default function activityTimeline(data) {
 
     /*
       plots the observations for each entity within the current entity type group. The
-      radius of the obervation point is based on the total value of all the integers 
-      included in the observation data pair. Each observation point is scaled linearly 
-      on the x-axis using xScalar defined above, and its timestamp is added as a data 
+      radius of the obervation point is based on the total value of all the integers
+      included in the observation data pair. Each observation point is scaled linearly
+      on the x-axis using xScalar defined above, and its timestamp is added as a data
       attribute for debugging purposes.
      */
     row.selectAll("circle")
        .data((observation, index) => { return entityData.obs[index]})
        .enter().append("circle")
-       .attr("cx", (observation, index) => { 
+       .attr("cx", (observation, index) => {
           let timestamp = new Date(data.timestamps[index]);
           return xScalar(timestamp.getTime()) + OFFSET
         })
