275165950feeefb14ee9099d374d422dfef73016
Mark Nutter <marknutter@gmail.com>
2017-08-24 12:06:35 -0500

    fixes to dynamic navigation logic.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -22,15 +22,7 @@ import unitSummary from './visualizations/unitSummary';
 import socialGraph from './visualizations/socialGraph';
 import key from 'keyboard-shortcut';
 
-let intervalCode = setInterval(function(){
 
-  let foundationEl = document.querySelector("#foundation");
-
-  if (!foundationEl) {
-    return;
-  } else {
-    clearInterval(intervalCode);
-  }
 
   key('ctrl shift a', function (e) {
     store.dispatch(toggleAnonymizer());
@@ -42,168 +34,217 @@ let intervalCode = setInterval(function(){
   let schoolId = getSchoolId();
   if (classroomId === "725" || classroomId === "676" || classroomId === "1289" || !isProduction()) {
 
-    let dynamicPrimaryNav = document.createElement("a");
-    dynamicPrimaryNav.className = "primary-nav-link";
-    dynamicPrimaryNav.id = "sensors-nav";
-    dynamicPrimaryNav.href = `/s/${schoolId}/networks/wf/events/sensors${classroomId ? '?classroom_id=' + classroomId : '' }`;
-    dynamicPrimaryNav.innerHTML = `<i class="fa fa-cubes"></i><span>Sensors</span>`
-    let insertionPoint = document.querySelector('.primary-nav');
-    insertionPoint && insertionPoint.appendChild(dynamicPrimaryNav);
+    
 
-    if (location.pathname.indexOf('wf/events') !== -1) {
-      dynamicPrimaryNav.classList.add('active');
-      let dynamicSecondaryNav = document.createElement("div");
-      dynamicSecondaryNav.className = "secondary-nav-link";
-      let secondaryNav = document.querySelector('.secondary-nav');
-      secondaryNav && secondaryNav.insertBefore(dynamicSecondaryNav, document.querySelector('.secondary-nav .clear'))
-      ReactDOM.render(
-        <SubNav/>,
-        dynamicSecondaryNav
-      )
-    }
+    let attempts = 0;
+    let foundationEl, insertionPoint;
 
-    if (location.pathname.indexOf('wf/events/sensors') !== -1) {
-      document.title = 'Sensors';
+    
+    setupDom('#foundation')
+      .then(setupSensei)
+      .catch(console.log);
 
-      ReactDOM.render(
-        <Provider store={store}>
-          <SensorMappingInterfaceContainer/>
-        </Provider>,
-        foundationEl
-      );
+    setupDom('.primary-nav')
+      .then(setupNavs)
+      .catch(console.log);
 
-      store.dispatch(fetchChildren());
-      store.dispatch(fetchTeachers());
-      store.dispatch(fetchEntities('areas'));
-      store.dispatch(fetchEntities('materials'));
-      store.dispatch(fetchMappings());
-
-    }
-
-    if (location.pathname.indexOf('wf/events/entities') !== -1) {
-      document.title = 'Manage Materials & Areas';
-
-      ReactDOM.render(
-        <Provider store={store}>
-          <ManageEntitiesInterfaceContainer/>
-        </Provider>,
-        foundationEl
-      );
-
-      store.dispatch(fetchEntities('areas'));
-      store.dispatch(fetchEntities('materials'));
-
-    }
-
-    if (location.pathname.indexOf('wf/events/insights') !== -1) {
-
-
-
-      
-      foundationEl.innerHTML = `
-        <div class='row'>
-          <div class='col-md-2' id='insights-nav-container'></div>
-          <div class='col-md-10'>
-            <h2 id='visualization-title'></h2>
-            <hr />
-            <div id='visualization'><svg></svg></div>
-          </div>
-        </div>
-      `;
-
-      ReactDOM.render(
-        <Provider store={store}>
-          <ActivityTimelineControlsContainer/>
-        </Provider>,
-        document.getElementById('insights-nav-container')
-      );
-
-
-      store.dispatch(fetchChildren());
-      store.dispatch(fetchTeachers());
-      store.dispatch(fetchEntities('areas'));
-      store.dispatch(fetchEntities('materials'));
-
-      let prevEntityUid, prevDate, prevEndDate, prevVisualization, prevInteractionType, prevZoom;
-
-      store.subscribe(() => {
-        let state = store.getState();
-        let entityId = _.get(state, 'insights.ui.currentEntityId');
-        let entityType = _.get(state, 'insights.ui.currentEntityType');
-        let visualization = _.get(state, 'insights.ui.visualization');
-        let interactionType = _.get(state, 'insights.ui.interactionType');
-        let entityUid = `${entityType}-${entityId}`
-        let date = _.get(state, 'insights.ui.currentDate');
-        let endDate = _.get(state, 'insights.ui.endDate');
-        let zoom = _.get(state, 'insights.ui.zoom');
-        let status = _.get(state, 'insights.status');
-
-        if (entityId && entityType && date && visualization && (endDate && _.includes(['studentSummary', 'interactionTotals'], visualization) || !_.includes(['studentSummary', 'interactionTotals'], visualization))) {
-          if (entityUid === prevEntityUid && date === prevDate && endDate === prevEndDate && prevVisualization === visualization && prevInteractionType === interactionType && prevZoom === zoom) {
-            let entity = _.get(state, `entities.${entityInflections[entityType]}.${entityId}`);
-            let dateString = (new Date(date)).toDateString();
-            if (endDate) {
-              dateString += ` to ${(new Date(endDate)).toDateString()}`
-            }
-            document.querySelector("#visualization-title").innerHTML = `${entity.displayName} <small>${dateString}</small>`
-            let observationsData = state.insights.observations[entityUid];
-            if (observationsData && (!_.isEmpty(observationsData.entities) && !_.isEmpty(observationsData.timestamps))) {
-              switch(visualization) {
-                case 'activityTimeline':
-                  activityTimeline(observationsData);
-                  break;
-                case 'segmentedTimeline':
-                  segmentedTimeline(observationsData);
-                  break;
-                case 'interactionTotals':
-                  interactionTotals(observationsData);
-                  break;
-                case 'unitSummary':
-                  unitSummary(observationsData);
-                  break;
-                case 'studentSummary':
-                  studentSummary(observationsData);
-                  break;
-                case 'socialGraph':
-                  socialGraph(observationsData);
-                  break;
-              }
-            } else {
-              if (status === 'fetched') {
-                document.querySelector("#visualization").innerHTML = '<h3>No data</h3>';
-              }
-            }
+    function setupDom(selector) {
+      let element;
+      let attempts = 0;
+      return new Promise((resolve, reject) => {
+        let intervalCode = setInterval(function(){
+          if (attempts > 10) {
+            clearInterval(intervalCode);
+            reject(`'${selector}' not found`);
           } else {
-            document.querySelector("#visualization").innerHTML = '<h3>loading...</h3>';
-            document.querySelector("#visualization-title").innerHTML = '';
-            switch(visualization) {
-              case 'activityTimeline':
-                store.dispatch(fetchObservations(entityId, entityType, date, interactionType));
-                break;
-              case 'segmentedTimeline':
-                store.dispatch(fetchInteractionPeriods(entityId, entityType, date));
-                break;
-              case 'socialGraph':
-                store.dispatch(fetchInteractionTotals(null, null, date, endDate));
-              case 'interactionTotals':
-              case 'unitSummary':
-              case 'studentSummary':
-                if (endDate && !(visualization === 'unitSummary' && !interactionType)) {
-                  store.dispatch(fetchInteractionTotals(entityId, entityType, date, endDate, visualization === 'unitSummary' && interactionType));
-                } else {
-                  document.querySelector("#visualization").innerHTML = '';
-                }
-                break;
+            element = document.querySelector(selector);
+            if (element) {
+              clearInterval(intervalCode);
+              resolve(element)
+            } else {
+              attempts++;
+              return;
             }
           }
-          prevVisualization = visualization;
-          prevInteractionType = interactionType;
-          prevDate = date;
-          prevEndDate = endDate;
-          prevEntityUid = entityUid;
-          prevZoom = zoom;
-        }
+        }, 1000);
       })
     }
+
+    function setupNavs(insertionPoint) {
+      let dynamicPrimaryNav = document.createElement("a");
+      dynamicPrimaryNav.className = "primary-nav-link";
+      dynamicPrimaryNav.id = "sensors-nav";
+      dynamicPrimaryNav.href = `/s/${schoolId}/networks/wf/events/sensors${classroomId ? '?classroom_id=' + classroomId : '' }`;
+      dynamicPrimaryNav.innerHTML = `<i class="fa fa-cubes"></i><span>Sensors</span>`
+      insertionPoint && insertionPoint.appendChild(dynamicPrimaryNav);
+
+      if (location.pathname.indexOf('wf/events') !== -1) {
+        dynamicPrimaryNav.classList.add('active');
+        let dynamicSecondaryNav = document.createElement("div");
+        dynamicSecondaryNav.className = "secondary-nav-link";
+        let secondaryNav = document.querySelector('.secondary-nav');
+
+        // remove the "Network" tab from the secondary nav if it exists
+        let secondaryNavNetworkLink = document.querySelector('.secondary-nav .secondary-nav-link');
+        if (secondaryNavNetworkLink && _.includes(secondaryNavNetworkLink.innerText, 'Network')) {
+          secondaryNavNetworkLink.remove();
+        }
+
+        secondaryNav && secondaryNav.insertBefore(dynamicSecondaryNav, document.querySelector('.secondary-nav .clear'))
+        ReactDOM.render(
+          <SubNav/>,
+          dynamicSecondaryNav
+        )
+      }
+    }
+
+    function setupSensei(foundationEl) {
+      if (location.pathname.indexOf('wf/events/sensors') !== -1) {
+        document.title = 'Sensors';
+
+        ReactDOM.render(
+          <Provider store={store}>
+            <SensorMappingInterfaceContainer/>
+          </Provider>,
+          foundationEl
+        );
+
+        store.dispatch(fetchChildren());
+        store.dispatch(fetchTeachers());
+        store.dispatch(fetchEntities('areas'));
+        store.dispatch(fetchEntities('materials'));
+        store.dispatch(fetchMappings());
+
+      }
+
+      if (location.pathname.indexOf('wf/events/entities') !== -1) {
+        document.title = 'Manage Materials & Areas';
+
+        ReactDOM.render(
+          <Provider store={store}>
+            <ManageEntitiesInterfaceContainer/>
+          </Provider>,
+          foundationEl
+        );
+
+        store.dispatch(fetchEntities('areas'));
+        store.dispatch(fetchEntities('materials'));
+
+      }
+
+      if (location.pathname.indexOf('wf/events/insights') !== -1) {
+
+
+
+        
+        foundationEl.innerHTML = `
+          <div class='row'>
+            <div class='col-md-2' id='insights-nav-container'></div>
+            <div class='col-md-10'>
+              <h2 id='visualization-title'></h2>
+              <hr />
+              <div id='visualization'><svg></svg></div>
+            </div>
+          </div>
+        `;
+
+        ReactDOM.render(
+          <Provider store={store}>
+            <ActivityTimelineControlsContainer/>
+          </Provider>,
+          document.getElementById('insights-nav-container')
+        );
+
+
+        store.dispatch(fetchChildren());
+        store.dispatch(fetchTeachers());
+        store.dispatch(fetchEntities('areas'));
+        store.dispatch(fetchEntities('materials'));
+
+        let prevEntityUid, prevDate, prevEndDate, prevVisualization, prevInteractionType, prevZoom;
+
+        store.subscribe(() => {
+          let state = store.getState();
+          let entityId = _.get(state, 'insights.ui.currentEntityId');
+          let entityType = _.get(state, 'insights.ui.currentEntityType');
+          let visualization = _.get(state, 'insights.ui.visualization');
+          let interactionType = _.get(state, 'insights.ui.interactionType');
+          let entityUid = `${entityType}-${entityId}`
+          let date = _.get(state, 'insights.ui.currentDate');
+          let endDate = _.get(state, 'insights.ui.endDate');
+          let zoom = _.get(state, 'insights.ui.zoom');
+          let status = _.get(state, 'insights.status');
+
+          if (entityId && entityType && date && visualization && (endDate && _.includes(['studentSummary', 'interactionTotals'], visualization) || !_.includes(['studentSummary', 'interactionTotals'], visualization))) {
+            if (entityUid === prevEntityUid && date === prevDate && endDate === prevEndDate && prevVisualization === visualization && prevInteractionType === interactionType && prevZoom === zoom) {
+              let entity = _.get(state, `entities.${entityInflections[entityType]}.${entityId}`);
+              let dateString = (new Date(date)).toDateString();
+              if (endDate) {
+                dateString += ` to ${(new Date(endDate)).toDateString()}`
+              }
+              document.querySelector("#visualization-title").innerHTML = `${entity.displayName} <small>${dateString}</small>`
+              let observationsData = state.insights.observations[entityUid];
+              if (observationsData && (!_.isEmpty(observationsData.entities) && !_.isEmpty(observationsData.timestamps))) {
+                switch(visualization) {
+                  case 'activityTimeline':
+                    activityTimeline(observationsData);
+                    break;
+                  case 'segmentedTimeline':
+                    segmentedTimeline(observationsData);
+                    break;
+                  case 'interactionTotals':
+                    interactionTotals(observationsData);
+                    break;
+                  case 'unitSummary':
+                    unitSummary(observationsData);
+                    break;
+                  case 'studentSummary':
+                    studentSummary(observationsData);
+                    break;
+                  case 'socialGraph':
+                    socialGraph(observationsData);
+                    break;
+                }
+              } else {
+                if (status === 'fetched') {
+                  document.querySelector("#visualization").innerHTML = '<h3>No data</h3>';
+                }
+              }
+            } else {
+              document.querySelector("#visualization").innerHTML = '<h3>loading...</h3>';
+              document.querySelector("#visualization-title").innerHTML = '';
+              switch(visualization) {
+                case 'activityTimeline':
+                  store.dispatch(fetchObservations(entityId, entityType, date, interactionType));
+                  break;
+                case 'segmentedTimeline':
+                  store.dispatch(fetchInteractionPeriods(entityId, entityType, date));
+                  break;
+                case 'socialGraph':
+                  store.dispatch(fetchInteractionTotals(null, null, date, endDate));
+                case 'interactionTotals':
+                case 'unitSummary':
+                case 'studentSummary':
+                  if (endDate && !(visualization === 'unitSummary' && !interactionType)) {
+                    store.dispatch(fetchInteractionTotals(entityId, entityType, date, endDate, visualization === 'unitSummary' && interactionType));
+                  } else {
+                    document.querySelector("#visualization").innerHTML = '';
+                  }
+                  break;
+              }
+            }
+            prevVisualization = visualization;
+            prevInteractionType = interactionType;
+            prevDate = date;
+            prevEndDate = endDate;
+            prevEntityUid = entityUid;
+            prevZoom = zoom;
+          }
+        })
+      }
+    }
+
+    
   }
-}, 1000);
+
+
