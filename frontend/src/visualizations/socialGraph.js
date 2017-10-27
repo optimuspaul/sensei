2a64236e25ad85ca7d3f36ffd01fa71008204715
Mark Nutter <marknutter@gmail.com>
2017-10-26 22:14:27 -0500

    zoom working, lines and attraction honor values

--- frontend/src/visualizations/socialGraph.js
+++ frontend/src/visualizations/socialGraph.js
@@ -10,20 +10,27 @@ export default function socialGraph(data) {
     return
   }
 
-  
+  data = JSON.parse(`{"entities":[["child",10642,"child",35273],["child",35271,"child",35273],["child",35272,"child",35273],["child",35268,"child",35273],["child",35270,"child",35273],["child",35273,"child",10642],["child",35271,"child",35269],["child",10642,"child",35269],["child",35268,"child",35269],["child",35272,"child",35269],["child",35270,"child",35269],["child",35273,"child",35271],["child",10642,"child",35271],["child",35272,"child",35271],["child",35268,"child",35271],["child",35269,"child",35271],["child",35270,"child",35271],["child",10642,"child",35272],["child",35271,"child",35272],["child",10642,"child",35268],["child",35271,"child",35268],["child",35272,"child",35268],["child",35269,"child",35268],["child",35273,"child",35268],["child",35270,"child",35268],["child",35271,"child",35270],["child",10642,"child",35270],["child",35272,"child",35270],["child",35273,"child",35270],["child",35268,"child",35270],["child",35269,"child",35270],["child",35268,"child",10642],["child",35272,"child",10642],["child",35271,"child",10642],["child",35270,"child",10642],["child",35269,"child",10642],["child",35268,"child",35272],["child",35269,"child",35272],["child",35273,"child",35272],["child",35270,"child",35272]],"obs":[1810,940,5560,880,330,1810,1730,1560,1710,3310,960,940,2080,1110,730,1730,860,2940,1110,1020,730,3060,1710,880,1520,860,1000,1670,330,1520,960,1020,2940,2080,1000,1560,3060,3310,5560,1670],"timestamps":["2017-07-01T05:00:00Z","2017-10-25T05:00:00Z"]}`)
 
   let state = store.getState();
   let storeEntities = state.entities;
+  let zoom = parseInt(_.get(store.getState(), "insights.ui.zoom")) || 1;
   let canvasWidth = 1000;
   let canvasHeight = 800;
   let currentEntityId = _.get(state, 'insights.ui.currentEntityId');
   let currentEntityType = _.get(state, 'insights.ui.currentEntityType');
 
-  document.querySelector("#visualization").innerHTML = `<svg id="social-graph" height=${canvasHeight} width=${canvasWidth}></svg>`;
+  document.querySelector("#visualization").innerHTML = `<svg id="social-graph" height=${canvasHeight} width=${canvasWidth}><g id="container"></g></svg>`;
 
   let scalar = d3.scaleLinear()
-    .domain([0, d3.max(data.obs)])
-    .range([0, 20]);
+    .domain([d3.min(data.obs), d3.max(data.obs)+d3.deviation(data.obs)])
+    .range([0, 20]).clamp(true);
+
+  let scaleTwenty = d3.scaleLinear().domain([0, 20])
+  let scaleZoom = d3.scaleLinear().domain([1,5])
+
+  let zoomLevel = scaleZoom.range([2,5])(zoom);
+
 
   let graphData = _.reduce(data.entities, (current, val, index) => {
     let force = parseInt(scalar(data.obs[index]), 10);
@@ -40,33 +47,43 @@ export default function socialGraph(data) {
    }, {nodes:[], links: [], bilinks: []})
 
 
-  var svg = d3.select("#visualization svg");
+  var svg = d3.select("#visualization svg g#container")
+  svg.attr("transform", `scale(${zoomLevel})`);
 
   var color = d3.scaleOrdinal(d3.schemeCategory20);
 
   var mb = d3.forceManyBody();
 
-  mb.strength(() => { return -200 });
+  mb.strength((d) => { 
+    return d3.scaleLinear().domain([0, 20]).range([-300,0])(d.value)
+  });
 
   var simulation = d3.forceSimulation()
-      .force("link", d3.forceLink().distance(10).strength(0.03))
+      .force("link", d3.forceLink().distance(calcDistance).strength(calcStrength))
       .force("charge", mb)
-      .force("center", d3.forceCenter(canvasWidth / 2, canvasHeight / 2));
+      .force("center", d3.forceCenter(canvasWidth / (2*zoomLevel), canvasHeight / (2*zoomLevel)));
 
   var nodeById = d3.map(graphData.nodes, function(d) { return d.id; });
 
   graphData.links.forEach(function(link) {
     var s = link.source = nodeById.get(link.source),
         t = link.target = nodeById.get(link.target),
-        i = {}; // intermediate node
+        i = {value: link.value}; // intermediate node
+        s.value = link.value;
+        t.value = link.value;
     graphData.nodes.push(i);
-    graphData.links.push({source: s, target: i}, {source: i, target: t});
+    graphData.links.push({source: s, target: i, value: link.value}, {source: i, target: t, value: link.value});
     graphData.bilinks.push([s, i, t]);
   });
 
   var link = svg.selectAll(".link")
     .data(graphData.bilinks)
     .enter().append("path")
+      .attr("style", function(d) { 
+        
+        return `stroke-width: ${scaleTwenty.range([scaleZoom.range([1,0.3])(zoom), scaleZoom.range([5,3])(zoom)])(d[0].value)};
+                stroke: rgba(24,140,99,${scaleTwenty.range([-0.1,1.5])(d[0].value)})`; 
+      })
       .attr("class", "link");
 
 
@@ -75,7 +92,7 @@ export default function socialGraph(data) {
     .data(graphData.nodes.filter(function(d) { return d.id; }))
     .enter().append("circle")
       .attr("class", "node")
-      .attr("r", 5)
+      .attr("r", scaleZoom.range([2,1])(zoom))
       .attr("fill", function(d) { return color(d.group); })
       .call(d3.drag()
           .on("start", dragstarted)
@@ -90,7 +107,7 @@ export default function socialGraph(data) {
       .data(graphData.nodes)
       .enter().append("text")
       .text(node => node.label)
-      .attr('font-size', 15)
+      .attr('style', `font-size: ${scaleZoom.range([5,2])(zoom)}px`)
       .call(d3.drag()
           .on("start", dragstarted)
           .on("drag", dragged)
@@ -116,15 +133,24 @@ export default function socialGraph(data) {
   function ticked() {
     link.attr("d", positionLink);
 
+    
+
     node.attr("transform", (d) => {
       return "translate(" + d.x + "," + d.y + ")";
     });
 
     label.attr("transform", (d) => {
-      return "translate(" + d.x+10 + "," + d.y+6 + ")";
+      return "translate(" + d.x+30 + "," + d.y+20 + ")";
     });
   }
 
+  function calcStrength(d) {
+    return d3.scaleLinear().domain([0, 20]).range([0.5,1])(d.value);
+  }
+
+  function calcDistance(d) {
+    return d3.scaleLinear().domain([0, 20]).range([50, 0])(d.value);
+  }
 
   function dragstarted(d) {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
