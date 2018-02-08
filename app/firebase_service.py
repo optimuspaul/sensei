84a4daba091f2ba65995e434c613f62a398aa0ef
Mark Nutter <marknutter@gmail.com>
2018-02-08 14:40:17 -0600

    firebase service"

---
+++ app/firebase_service.py
@@ -0,0 +1,16 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from firebase_admin import firestore
+
+
+
+class FirebaseService:
+  
+  def __init__(self, databaseURL, cert_path):
+    cred = credentials.Certificate(cert_path)
+    firebase_admin.initialize_app(cred, options={
+        'databaseURL': databaseURL
+    })
+    self.db = firebase_admin.firestore.client();
+
