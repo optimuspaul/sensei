137939f51ea26aee2af5f84fef00fa01716618a6
Pete Schwamb <pete@schwamb.net>
2018-03-01 19:44:06 -0600

    Add position information to area model (#110)
    * Add position information to area model
    
    * updated Area schema in api docs

--- app/api/areas.py
+++ app/api/areas.py
@@ -18,7 +18,12 @@ def areas_index():
 def create_area():
     if not request.json or not 'name' in request.json or not 'classroom_id' in request.json:
         abort(400)
-    area = Area(request.json.get('classroom_id'), request.json.get('name'))
+    area = Area(
+      request.json.get('classroom_id'),
+      request.json.get('name'),
+      request.json.get('x_position'),
+      request.json.get('y_position'),
+      request.json.get('z_position'))
     db.session.add(area)
     db.session.commit()
     return jsonify( area.as_dict() ), 201
@@ -37,5 +42,8 @@ def delete_area(id):
 def update_area(id):
     area = Area.query.get(id)
     area.name = request.json.get('name', area.name)
+    area.x_position = request.json.get('x_position', area.x_position)
+    area.y_position = request.json.get('y_position', area.y_position)
+    area.z_position = request.json.get('z_position', area.z_position)
     db.session.commit()
     return jsonify( area.as_dict() )
